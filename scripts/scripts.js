22bcc4d072a4562db0b6a3c6cd0606a3e593f5c5
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2020-11-09 01:44:13 -0300

    Avance para cambiar el color del indicador de la bomba

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,13 @@
-function ejecutarAccion(ref, bomba, formId){
+function ejecutarAccion(ref, bomba, formId,light){
  /*var form = $(ref).parent();
  var hval = form.find('type[hidden]').val();
  var fid = form.attr('id');
  */
- regarBomba(bomba, formId);
+ regarBomba(bomba, formId, light);
 
 }
 
-function regarBomba(bomba, formId){
+function regarBomba(bomba, formId, light){
   let x = document.getElementById(bomba);
   //console.log(x.value);
   if (x.value==1) {
@@ -23,14 +23,43 @@ function regarBomba(bomba, formId){
   //https://serverregistrodatos.000webhostapp.com/
   $.post("updatesettings.php",
     data,
-    function(info){
-      console.log(info);
-      $("#result").html(info);
+    function(result){
+      console.log(result);
+      //console.alert(result);
+      reRenderPageWithPhpEcho( result , light );
     }
   );
+  //window.location.reload();
+}
+
+
+function reRenderPageWithPhpEcho(echoJson,light){
+  let data = JSON.parse(echoJson);
+console.log(light);
+for(var i = 0; i < data.length; i++) {
+    var obj = data[i];
+
+if(obj.variable=="bomba1"){
+     console.log(obj.valor);
+     var divElement = document.getElementById( light );
+ if(obj.valor == 1){
+                       console.log("Bomba prendida");
+                       divElement.style.backgroundColor = '#a5d6a7';
+                      }
+                      else {
+                       divElement.style.backgroundColor = '#e57373';
+                       console.log("Bomba apagada");
+                      }
+}
+
+}
+
+
+
 }
 
 /*
 $("#formbomba1").submit( function() {
   return false;
-});*/
+});
+*/
--- updatesettings.php
+++ updatesettings.php
@@ -1,6 +1,12 @@
 <?php
 
-$humedadsuelomaxima=$_POST["humedadsuelomaxima"]?? null;
+/*
+foreach($_POST as $key => $value) {
+  echo "POST parameter '$key' has '$value'";
+}
+*/
+
+$humedadsuelomaxima=$_POST['humedadsuelomaxima']??null;
 $humedadsuelominima=$_POST["humedadsuelominima"] ?? null;
 $temperaturamaxima=$_POST["temperaturamaxima"]?? null;
 $temperaturaminima=$_POST["temperaturaminima"]?? null;
@@ -15,35 +21,39 @@ $data = json_decode($jsonString, true);
 
 
 foreach ($data as $key => $entry) {
- 
+
     switch ($entry['variable']) {
 
       case 'humedadsuelomaxima':
-                  if(!is_null("$humedadsuelomaxima")) $data[$key]['valor'] = $humedadsuelomaxima;
+                  if (isset($_POST['humedadsuelomaxima']) && $_POST['humedadsuelomaxima'] != ""){
+                      $data[$key]['valor'] = $humedadsuelomaxima;}
                   break;
       case 'humedadsuelominima':
-                  if(!is_null("$humedadsuelominima")) $data[$key]['valor'] = $humedadsuelominima;
+                  if (isset($_POST['humedadsuelominima']) && $_POST['humedadsuelominima'] != ""){ $data[$key]['valor'] = $humedadsuelominima;}
                   break;
       case 'temperaturamaxima':
-                  if(!is_null("$temperaturamaxima")) $data[$key]['valor'] = $temperaturamaxima;
+                  if (isset($_POST['temperaturamaxima']) && $_POST['temperaturamaxima'] != ""){ $data[$key]['valor'] = $temperaturamaxima;}
                   break;
       case 'temperaturaminima':
-                  if(!is_null("$temperaturaminima")) $data[$key]['valor'] = $temperaturaminima;
+                  if (isset($_POST['temperaturaminima']) && $_POST['temperaturaminima'] != ""){ $data[$key]['valor'] = $temperaturaminima;}
                   break;
       case 'humedadambientemaxima':
-                  if(!is_null("$humedadambientemaxima")) $data[$key]['valor'] = $humedadambientemaxima;
+                  if (isset($_POST['humedadambientemaxima']) && $_POST['humedadambientemaxima'] != ""){ $data[$key]['valor'] = $humedadambientemaxima;}
                   break;
       case 'humedadambienteminima':
-                  if(!is_null("$humedadambienteminima")) $data[$key]['valor'] = $humedadambienteminima;
+                  if (isset($_POST['humedadambienteminima']) && $_POST['humedadambienteminima'] != ""){ $data[$key]['valor'] = $humedadambienteminima;}
                   break;
       case 'bomba1':
-                  if(!is_null($bomba1)) $data[$key]['valor'] = $bomba1;
+                  if (isset($_POST['bomba1']) && $_POST['bomba1'] != ""){
+                      $data[$key]['valor'] = $bomba1;}
                   break;
       case 'bomba2':
-                  if(!is_null($bomba2)) $data[$key]['valor'] = $bomba2;
+                  if (isset($_POST['bomba2']) && $_POST['bomba2'] != ""){
+                      $data[$key]['valor'] = $bomba2;}
                   break;
       case 'bomba3':
-                  if(!is_null($bomba3)) $data[$key]['valor'] = $bomba3;
+                  if (isset($_POST['bomba3']) && $_POST['bomba3'] != ""){
+                      $data[$key]['valor'] = $bomba3;}
                   break;
 
     }
